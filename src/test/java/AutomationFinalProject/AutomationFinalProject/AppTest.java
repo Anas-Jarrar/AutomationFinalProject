c972fb1b19263b4264eea86e9e6bccc7e2522ec3
Anas-Jarrar <anasjarrar944@gmail.com>
2025-05-05 03:57:33 +0300

    Last Update

--- src/test/java/AutomationFinalProject/AutomationFinalProject/AppTest.java
+++ src/test/java/AutomationFinalProject/AutomationFinalProject/AppTest.java
@@ -5,27 +5,21 @@ import java.time.LocalDate;
 import java.util.Random;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
-public class AppTest {
-
-	String TheWebSite = "https://www.almosafer.com/en";
-	WebDriver driver = new ChromeDriver();
+public class AppTest extends TestData {
 
 	@BeforeTest
 	public void MySetup() {
-		driver.manage().window().maximize();
-		driver.get(TheWebSite);
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-		WebElement CountryButton = driver
-				.findElement(By.cssSelector(".sc-jTzLTM.cta__button.cta__saudi.btn.btn-primary"));
-		CountryButton.click();
+		Setup();
 	}
 
 	@Test(priority = 1, enabled = false)
@@ -40,23 +34,17 @@ public class AppTest {
 		// we can use cssSelector also
 		String ActualCurrency = driver.findElement(By.xpath("//button[@data-testid='Header__CurrencySelector']"))
 				.getText();
-		String ExpectedCurrency = "SAR";
 		Assert.assertEquals(ActualCurrency, ExpectedCurrency);
-
 	}
 
 	@Test(priority = 3, enabled = false)
 	public void CheckContactNumber() {
 		String ActulContacteNum = driver.findElement(By.linkText("+966554400000")).getText();
-		String ExpectedContacteNum = "+966554400000";
 		Assert.assertEquals(ActulContacteNum, ExpectedContacteNum);
-
 	}
 
 	@Test(priority = 4, enabled = false)
 	public void CheckQitafLogo() {
-//		boolean ActualLogo=driver.findElement(By.cssSelector(".sc-bdVaJa.bxRSiR.sc-lcpuFF.jipXfR")).isDisplayed();
-
 		WebElement TheFotter = driver.findElement(By.tagName("footer"));
 		boolean ActualLogoDisplayed = TheFotter.findElement(By.cssSelector(".sc-ekulBa.iOOTo"))
 				.findElement(By.cssSelector(".sc-bdVaJa.bxRSiR.sc-lcpuFF.jipXfR")).isDisplayed();
@@ -66,64 +54,81 @@ public class AppTest {
 
 	@Test(priority = 5, enabled = false)
 	public void StaysTabIsNotSelectedByDefault() throws InterruptedException {
-
 		Thread.sleep(2000);
-
 		String currentURL = driver.getCurrentUrl();
 		boolean isHotelsSelected = currentURL.contains("hotels-home");
-
 		Assert.assertFalse(isHotelsSelected);
-
 	}
 
 	@Test(priority = 6, enabled = false)
 	public void WelcomPageTapSelectedCheck() {
 		WebElement StayTap = driver.findElement(By.id("uncontrolled-tab-example-tab-hotels"));
 		String ActualValue = StayTap.getDomAttribute("aria-selected");
-		String ExpectedValue = "false";
 		Assert.assertEquals(ActualValue, ExpectedValue);
-
 	}
 
-	@Test(priority = 7,enabled=false)
+	@Test(priority = 7, enabled = false)
 	public void DepartureDateCheck() {
-
-		LocalDate TomorrowDate = LocalDate.now().plusDays(1);
 		WebElement DepartureDateField = driver.findElement(By.cssSelector(".sc-bYnzgO.sc-cPuPxo.jNskcH"));
 		String ActualDepartureDay = DepartureDateField.findElement(By.cssSelector(".sc-dXfzlN.iPVuSG")).getText();
 		String ActualDepartureMonth = DepartureDateField.findElement(By.cssSelector(".sc-eSePXt.ljMnJa")).getText();
-		String FormatedTomorrowDate=String.format("%02d", TomorrowDate.getDayOfMonth());
 		Assert.assertEquals(ActualDepartureDay, FormatedTomorrowDate);
 		Assert.assertEquals(ActualDepartureMonth.toUpperCase(), TomorrowDate.getMonth().toString());
 	}
 
-	@Test(priority = 8,enabled=false)
+	@Test(priority = 8, enabled = false)
 	public void ReturnDateCheck() {
-
-		LocalDate AfterTomorrowDate = LocalDate.now().plusDays(2);
 		WebElement ReturnDateField = driver.findElement(By.cssSelector(".sc-bYnzgO.sc-hvvHee.aiGEY"));
 		String ActualReturnDay = ReturnDateField.findElement(By.cssSelector(".sc-dXfzlN.iPVuSG")).getText();
 		String ActualReturnMonth = ReturnDateField.findElement(By.cssSelector(".sc-eSePXt.ljMnJa")).getText();
-		String FormatedAfterTomorrowDate=String.format("%02d", AfterTomorrowDate.getDayOfMonth());
 		Assert.assertEquals(ActualReturnDay, FormatedAfterTomorrowDate);
 		Assert.assertEquals(ActualReturnMonth.toUpperCase(), AfterTomorrowDate.getMonth().toString());
-
 	}
-	@Test (priority=9,invocationCount = 5)
+
+	@Test(priority = 9, enabled = true)
 	public void ChangeWebsiteLanguage() {
-		
-		String [] Websites= {"https://www.almosafer.com/en","https://www.almosafer.com/ar"};
-		Random rand=new Random();
-		int Index=rand.nextInt(Websites.length);
-		driver.get(Websites[Index]);		
-		String CurrentUrl=driver.getCurrentUrl();
-		if(CurrentUrl.contains("en")) {
+		driver.get(Websites[Index]);
+		String CurrentUrl = driver.getCurrentUrl();
+		if (CurrentUrl.contains("en")) {
 			CheckWebsiteLanguage("en");
-		}
-		else {
+		} else {
 			CheckWebsiteLanguage("ar");
 		}
-		
+
+	}
+
+	@Test(priority = 10)
+	public void RandomSelectCity() {
+		WebElement StaysButton = driver.findElement(By.id("uncontrolled-tab-example-tab-hotels"));
+		StaysButton.click();
+		String CurrentUrl = driver.getCurrentUrl();
+		WebElement SearchBar = driver.findElement(By.cssSelector(".sc-phbroq-2.uQFRS.AutoComplete__Input"));
+		if (CurrentUrl.contains("en")) {
+			SearchBar.sendKeys(EnCities[RandomEnCityIndex]);
+		} else {
+			SearchBar.sendKeys(ArCities[RandomArCityIndex]);
+		}
+
+	}
+
+	@Test(priority = 11)
+	public void RandomSelectRooms() {
+		WebElement ReservationRoomSelect = driver.findElement(By.cssSelector(".sc-tln3e3-1.gvrkTi"));
+		Select Myselect = new Select(ReservationRoomSelect);
+		Myselect.selectByValue(Values[RoomsRandomValue]);
+		driver.findElement(By.cssSelector(".sc-1vkdpp9-5.btwWVk")).click();
+	}
+
+	@Test(priority = 12)
+	public void CheckTheResultsIsRetrived() throws InterruptedException {
+		Thread.sleep(10000);
+		String CurrentUrl = driver.getCurrentUrl();
+		String Results = driver.findElement(By.xpath("//span[@data-testid='srp_properties_found']")).getText();
+		if (CurrentUrl.contains("en")) {
+			Assert.assertEquals(Results.contains("found"), true);
+		} else {
+			Assert.assertEquals(Results.contains("إقامة"), true);
+		}
 	}
 
 	@AfterTest
---
+++ src/test/java/AutomationFinalProject/AutomationFinalProject/TestData.java
@@ -0,0 +1,61 @@
+package AutomationFinalProject.AutomationFinalProject;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.util.Random;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestData {
+
+	String TheWebSite = "https://www.almosafer.com/en";
+	WebDriver driver = new ChromeDriver();
+	
+	String ExpectedCurrency = "SAR";//Test(2)
+	
+	String ExpectedContacteNum = "+966554400000";//Test(3)
+	
+	String ExpectedValue = "false";//Test(6)
+	
+
+	LocalDate TomorrowDate = LocalDate.now().plusDays(1);//Test(7)
+	String FormatedTomorrowDate = String.format("%02d", TomorrowDate.getDayOfMonth());//Test(7)
+	
+	LocalDate AfterTomorrowDate = LocalDate.now().plusDays(2);//Test(8)
+	String FormatedAfterTomorrowDate = String.format("%02d", AfterTomorrowDate.getDayOfMonth());//Test(8)
+	
+	String[] Websites = { "https://www.almosafer.com/en", "https://www.almosafer.com/ar" };//Test(9)
+	Random rand = new Random();//Test(9)
+	int Index = rand.nextInt(Websites.length);//Test(9)
+	
+	String[] EnCities = { "Dubai", "Jeddah", "Riyadh" };//Test(10)
+	int RandomEnCityIndex = rand.nextInt(EnCities.length);//Test(10)
+	String[] ArCities = { "جدة", "دبي" };//Test(10)
+	int RandomArCityIndex = rand.nextInt(ArCities.length);//Test(10)
+	
+	String[] Values = { "A", "B" };//Test(11)
+	int RoomsRandomValue = rand.nextInt(Values.length);//Test(11)
+
+	public void Setup() {
+		driver.manage().window().maximize();
+		driver.get(TheWebSite);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+		WebElement CountryButton = driver
+				.findElement(By.cssSelector(".sc-jTzLTM.cta__button.cta__saudi.btn.btn-primary"));
+		CountryButton.click();
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
